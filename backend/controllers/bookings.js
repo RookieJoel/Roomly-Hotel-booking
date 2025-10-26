9bf66d85fbb9d9b27eb19bd5d03c7844885bb397
TanontZ <iamtnont@gmail.com>
2025-10-26 14:27:44 +0700

    add remaining methods for booking CRUD and route booking

--- backend/controllers/bookings.js
+++ backend/controllers/bookings.js
@@ -1,5 +1,52 @@
 const Booking = require('../models/Booking');
 const Hotel = require('../models/Hotel');
+//@desc GET all bookings
+//@route GET /api/v1/bookings
+//@access Private
+exports.getBookings = async (req, res, next) => {
+    let query;
+    //General users can see only their bookings
+    if (req.user.role !== 'admin') {
+        query = Booking.find({ user: req.user.id }).populate({
+            path : 'hotel',
+            select : 'name address tel'
+        });
+    }else{
+        query = Booking.find().populate({
+            path : 'hotel',
+            select : 'name address tel'
+        });
+    }
+    try {
+        const bookings = await query;
+        return res.status(200).json({
+            success: true,
+            count: bookings.length,
+            data: bookings
+        });
+    } catch (error) {
+        console.log(error.stack);
+        return res.status(500).json({ success: false, error: 'Cannot find Booking' });
+    }
+};
+
+//@desc Get single booking
+//@route GET /api/v1/bookings/:id
+//@access Public
+exports.getBooking = async (req, res, next) => {
+    try {
+        const booking = await Booking.findById(req.params.id).populate({
+            path : 'hotel',
+            select : 'name address tel'
+        });
+        if (!booking) {
+            return res.status(404).json({ success: false , message : `No booking with the id of ${req.params.id}` });
+        }
+        res.status(200).json({ success: true, data: booking });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Cannot find Booking' });
+    }
+};
 
 //@desc add new booking
 //@route POST /api/v1/hotels/:hotelId/bookings
@@ -32,4 +79,47 @@ exports.addBooking = async (req, res, next) => {
     }
 };
 
+//@desc Update booking
+//@route PUT /api/v1/bookings/:id
+//@access Private
+exports.updateBooking = async (req, res, next) => {
+    try {
+        let booking = await Booking.findById(req.params.id);
+        if (!booking) {
+            return res.status(404).json({ success: false, message: `No booking with the id of ${req.params.id}` });
+        }
+        //make sure user is booking owner
+        if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to update this booking` });
+        }
+        booking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({ success: true, data: booking });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Cannot update Booking' });
+    }
+};
 
+//@desc Delete booking
+//@route DELETE /api/v1/bookings/:id
+//@access Private
+exports.deleteBooking = async (req, res, next) => {
+    try {
+        const booking = await Booking.findById(req.params.id);
+        if (!booking) {
+            return res.status(404).json({ success: false, message: `No booking with the id of ${req.params.id}` });
+        }
+        
+        //make sure user is booking owner
+        if (booking.user.toString() !== req.user.id && req.user.role !== 'admin') {
+            return res.status(401).json({ success: false, message: `User ${req.user.id} is not authorized to delete this booking` });
+        }
+        
+        await booking.deleteOne();
+        res.status(200).json({ success: true, data: {} });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Cannot delete Booking' });
+    }
+}
--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const {addBooking} = require('../controllers/bookings');
+const {getBookings, getBooking, addBooking, updateBooking, deleteBooking} = require('../controllers/bookings');
 
 const router = express.Router({mergeParams: true});
 
 const {protect,authorize} = require('../middleware/auth');
 
-router.route('/').post(protect,authorize('admin','user'), addBooking);
-
+router.route('/').get(protect, getBookings).post(protect,authorize('admin','user'), addBooking);
+router.route('/:id').get(protect, getBooking).put(protect,authorize('admin','user'), addBooking).delete(protect,authorize('admin','user'), deleteBooking);
 
 module.exports = router;
